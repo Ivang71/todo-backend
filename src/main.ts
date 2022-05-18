d2bb18c552bcb9ecec88c79ef8c638e32ac65a18
Artem Tolochkov <artemtolv@gmail.com>
2022-05-18 21:12:36 +0300

    Code refactor

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 import * as dotenv from 'dotenv'
 import { AppModule } from './app.module'
-import { LoggerService } from './logger.service'
 
 dotenv.config()
 
 const bootstrap = async () => {
-  const PORT = process.env.PORT || 5000
+  const port = process.env.PORT || 9000
   const options = { cors: true }
   const app = await NestFactory.create(AppModule, options)
 
-  app.useLogger(new LoggerService('Main'))
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
 
@@ -25,7 +23,7 @@ const bootstrap = async () => {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('/api/docs', app, document)
 
-  await app.listen(PORT, '0.0.0.0', () => console.log('server started'))
+  await app.listen(port, '0.0.0.0', () => console.log('server started'))
 }
 
 bootstrap()
